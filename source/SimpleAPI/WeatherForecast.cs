25debff4d9161b478c99ce6076d293ca41105d91
Prashanth Shetty <ds.prashanth@hpe.com>
2020-07-21 00:03:04 +0530

    Initial Commit

---
+++ source/SimpleAPI/WeatherForecast.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace SimpleAPI
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+
+        public object Get(int v)
+        {
+            return TemperatureF;
+        }
+    }
+}
---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecast controller = new WeatherForecast();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            WeatherForecast controller = new WeatherForecast();
+            var returnValue = controller.Get(1);
+            Assert.NotEqual("Prashanth", returnValue);
+            Assert.Equal(32, returnValue);
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
